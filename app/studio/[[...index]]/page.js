12117b722cada6c38a32a7199aa2878195aa3c15
Zdenko Markovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-08-31 20:12:57 +0200

    Updated page.js

--- app/studio/[[...index]]/page.js
+++ app/studio/[[...index]]/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { NextStudio } from 'next-sanity/studio/loading'
+import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
 export default function StudioPage() {
